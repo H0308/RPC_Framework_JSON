a5614ef2d70c975fdec379a0c55794a619f203af
H0308 <1848312235@qq.com>
2025-04-26 12:26:42 +0800

    UUID工具类

---
+++ rpc_framework/utils/uuid_generator.h
@@ -0,0 +1,26 @@
+#ifndef __rpc_uuid_generator_h__
+#define __rpc_uuid_generator_h__
+
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+namespace uuid_generator
+{
+    class Generator
+    {
+    public:
+        static std::string generate_uuid()
+        {
+            // 使用boost库生成uuid
+            // 创建一个随机数生成器
+            boost::uuids::random_generator generator;
+
+            // 生成一个随机 UUID
+            boost::uuids::uuid id = generator();
+            return boost::uuids::to_string(id);
+        }
+    };
+}
+
+#endif
